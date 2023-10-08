123d55e413c22e148104f92886c4944afb3c13c6
mg4 <gueyemor2009@gmail.com>
2023-10-08 08:50:16 +0000

    GG complete

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Distributed Systems Flyio
+
+Gossip Glommers in Rust
+
+https://fly.io/dist-sys
---
+++ src/bin/unique-ids.rs
@@ -0,0 +1,55 @@
+use gossip_glommers::*;
+
+use anyhow::Context;
+use serde::{Deserialize, Serialize};
+use std::io::{StdoutLock, Write};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum Payload {
+    Generate,
+    GenerateOk {
+        #[serde(rename = "id")]
+        guid: String,
+    },
+}
+
+struct UniqueNode {
+    node: String,
+    id: usize,
+}
+
+impl Node<(), Payload> for UniqueNode {
+    fn from_init(
+        _state: (),
+        init: Init,
+        _tx: std::sync::mpsc::Sender<Event<Payload>>,
+    ) -> anyhow::Result<Self> {
+        Ok(UniqueNode {
+            node: init.node_id,
+            id: 1,
+        })
+    }
+
+    fn step(&mut self, input: Event<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
+        let Event::Message(input) = input else {
+            panic!("got injected event when there's no event injection");
+        };
+
+        let mut reply = input.into_reply(Some(&mut self.id));
+        match reply.body.payload {
+            Payload::Generate => {
+                let guid = format!("{}-{}", self.node, self.id);
+                reply.body.payload = Payload::GenerateOk { guid };
+                reply.send(output).context("send response to generate")?;
+            }
+            Payload::GenerateOk { .. } => {}
+        }
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    main_loop::<_, UniqueNode, _, _>(())
+}
